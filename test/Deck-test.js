e58f4e45d86e17a7f015f0c3eaebb267eed0d67e
Kim McCaskill <kimmccaskill92@gmail.com>
2019-11-19 14:51:21 -0700

    Create instantiatable function

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,11 +1,19 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Deck = require('.../src/Deck');
+const Deck = require('../src/Deck');
 const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
-describe('Card', function() {
+describe('Deck', function() {
 
-  
-}
+  it('should be a function', function() {
+    const deck = new Deck();
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be an instance of Deck', function() {
+    const deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
+  });
+});
