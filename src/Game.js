217a1e928d84fdf59158ab91f27ab471c57f9a61
Kim McCaskill <kimmccaskill92@gmail.com>
2019-11-20 16:02:40 -0700

    Add game start function

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,14 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Round = require('../src/Round')
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = 0;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +18,15 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  start() {
+    // const card = new Card();
+    const deck = new Deck(prototypeQuestions);
+    const round = new Round(deck);
+
+    this.printMessage(deck, round);
+    this.printQuestion(round)
   }
 }
 
