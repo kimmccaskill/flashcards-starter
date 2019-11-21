6fb83e5fa51622d6acf40f93f84bfa684e78377a
Kim McCaskill <kimmccaskill92@gmail.com>
2019-11-21 14:42:48 -0700

    Refactor due to eslint

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,11 +5,9 @@ const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
 describe('Turn', function() {
-  let card1, card2, card3, turn;
+  let card1, turn;
   beforeEach(() => {
     card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
 
   });
 
